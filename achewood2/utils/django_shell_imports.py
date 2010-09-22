f766ffc34448f4d2b312fa630983a818e3638474
fish2000 <none@none>
2010-09-21 20:17:20 -0400

    fleshed django_shell_imports.py for e-z REPLing

---
+++ achewood2/utils/django_shell_imports.py
@@ -0,0 +1,26 @@
+
+## setup django
+from django.core.management import setup_environ
+import achewood2.settings
+setup_environ(settings)
+
+from django.conf import settings
+from django.db import models, connection
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ObjectDoesNotExist
+
+import datetime
+from imagekit.models import ImageModel
+from tagging.fields import TagField
+from tagging.models import Tag
+
+## import achewood2 specifics
+from achewood2.models import AWComic, AWImage
+from achewood2.utils import importstrips as strips
+
+## web scraping stuff
+from BeautifulSoup import BeautifulSoup
+import re, urllib2, string, os, sys
+
