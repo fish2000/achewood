ebc6b0a88ca3ac2724db73022b3088d8d94f28ff
fish2000 <none@none>
2010-09-21 22:42:48 -0400

    Fix admin links to alturls relative to achewood.com

--- achewood2/localache/admin.py
+++ achewood2/localache/admin.py
@@ -1,4 +1,4 @@
-import os
+import os, urlparse
 from django.conf import settings
 from django.db import models
 from django.contrib import admin
@@ -19,9 +19,10 @@ class AWComicAdmin(AWNimda):
 	search_fields = ['title', 'asseturlstring', 'alttext', 'dialogue']
 	def with_alturl(self, obj):
 		yesno = obj.alturl.startswith('http://m.assetbar.com/achewood/one_strip') and "img/admin/icon-no.gif" or "img/admin/icon-yes.gif"
+		alturl = obj.alturl.startswith('http://') and obj.alturl or urlparse.urljoin('http://www.achewood.com/', obj.alturl, allow_fragments=True)
 		return u'<a href="%s"><img src="%s" alt="Huh" /></a>' % (
 			#obj.id and ("/admin/localache/awcomic/%s/" % obj.id) or '#',
-			obj.alturl.startswith('http://m.assetbar.com/achewood/one_strip') and "#" or obj.alturl,
+			obj.alturl.startswith('http://m.assetbar.com/achewood/one_strip') and "#" or alturl,
 			os.path.join(settings.ADMIN_MEDIA_PREFIX, yesno),
 		)
 	with_alturl.short_description = "Alt URL?"
